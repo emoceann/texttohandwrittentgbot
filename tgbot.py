3f58b56757e079285555cb444034d4167720f388
emoceann <83538562+emoceann@users.noreply.github.com>
2021-10-08 15:55:40 +0500

    Create tgbot.py

---
+++ tgbot.py
@@ -0,0 +1,25 @@
+from pywhatkit import text_to_handwriting
+from pywhatkit import image_to_ascii_art
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+
+
+bot = Bot(token='There goes token')
+dp = Dispatcher(bot)
+
+
+
+
+
+@dp.message_handler()
+async def ascii_art_func(message : types.Message):
+    user_id = message.from_user.id
+    text_to_handwriting(message.text)
+    await bot.send_photo(user_id, photo=open('pywhatkit.png', 'r'))
+    
+    
+    
+    executor.start_polling(dp, skip_updates=True)
